d8bf2847872c9a7fb16b562f4ce959bc0d39f753
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-02-21 21:13:27 +0530

    implementing_edit_user_profile_details

--- src/components/expense-tracker/Profile.js
+++ src/components/expense-tracker/Profile.js
@@ -1,20 +1,45 @@
-import { useContext, useRef } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import classes from "./Profile.module.css";
 import AuthContex from "../contex/AuthContex";
 
 const Profile = () => {
   const authCtx = useContext(AuthContex);
-  const nameRef = useRef("");
-  const urlRef = useRef("");
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBMCRv6A2RlE36nAMBYrfJ-3aBiPdMCfZE",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setName(data.users[0].displayName);
+        setUrl(data.users[0].photoUrl);
+      });
+  }, []);
+
+  const fullNameHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const imageUrlHandler = (e) => {
+    setUrl(e.target.value);
+  };
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
 
-    const fullName = nameRef.current.value;
-    const URL = urlRef.current.value;
-
-    if (fullName.length == 0 || URL.length == 0) {
+    if (name.length == 0 || url.length == 0) {
       alert("please fill all fields");
       return;
     }
@@ -26,8 +51,8 @@ const Profile = () => {
           method: "POST",
           body: JSON.stringify({
             idToken: authCtx.token,
-            displayName: fullName,
-            photoUrl: URL,
+            displayName: name,
+            photoUrl: url,
             returnSecureToken: true,
           }),
           headers: {
@@ -38,13 +63,12 @@ const Profile = () => {
 
       const data = await res.json();
 
-      if(res.ok){
-        console.log(data);
-      }else{
+      if (res.ok) {
+      } else {
         throw new Error(data.error.message);
-      }      
+      }
     } catch (e) {
-        alert(e);
+      alert(e);
     }
   };
 
@@ -56,11 +80,21 @@ const Profile = () => {
           <form onSubmit={onSubmitHandler}>
             <div className="form-group">
               <label>Full Name</label>
-              <input ref={nameRef} type="text" className="form-control" />
+              <input
+                onChange={fullNameHandler}
+                value={name}
+                type="text"
+                className="form-control"
+              />
             </div>
             <div className="form-group">
               <label>Profile Photo URL</label>
-              <input ref={urlRef} type="url" className="form-control" />
+              <input
+                onChange={imageUrlHandler}
+                value={url}
+                type="url"
+                className="form-control"
+              />
             </div>
             <button type="submit" className="btn btn-primary">
               Update
