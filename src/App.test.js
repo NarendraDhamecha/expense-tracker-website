9af88839309cc049852dc86b9777b472306c3409
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-03-07 19:11:40 +0530

    testing_user_actions

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,102 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
 import ForgetPassword from "./components/Auth/ForgetPassword";
+import Expenses from "./components/expense-tracker/Expenses";
+import Profile from "./components/expense-tracker/Profile";
+import Store from "./redux-store/Store";
 
-describe('forget password', () => {
-    test('header', () => {
-        render(<ForgetPassword/>);
-        const headerTxt = screen.getByText('FORGET PASSWORD');
-        expect(headerTxt).toBeInTheDocument();
-    })
+describe("forget password component", () => {
+  test("header", () => {
+    render(<ForgetPassword />);
+    const headerTxt = screen.getByText("FORGET PASSWORD");
+    expect(headerTxt).toBeInTheDocument();
+  });
+
+  test("lable", () => {
+    render(<ForgetPassword />);
+    const lableTxt = screen.getByText(
+      "Enter the email with which you have registered."
+    );
+    expect(lableTxt).toBeInTheDocument();
+  });
+
+  test("button", () => {
+    render(<ForgetPassword />);
+    const buttonTxt = screen.getByText("Send Link");
+    expect(buttonTxt).toBeInTheDocument();
+  });
+});
+
+describe("Profile component", () => {
+  test("header", () => {
+    render(
+      <Provider store={Store}>
+        <Profile />
+      </Provider>
+    );
+
+    const Headertxt = screen.getByText("Contact Details");
+    expect(Headertxt).toBeInTheDocument();
+  });
+
+  test("profile photo lable", () => {
+    render(
+      <Provider store={Store}>
+        <Profile />
+      </Provider>
+    );
+
+    const profilePhotoTxt = screen.getByText("Profile Photo URL");
+    expect(profilePhotoTxt).toBeInTheDocument();
+  });
+
+  test("full name lable", () => {
+    render(
+      <Provider store={Store}>
+        <Profile />
+      </Provider>
+    );
+
+    const fullNameTxt = screen.getByText("Full Name");
+    expect(fullNameTxt).toBeInTheDocument();
+  });
+});
+
+describe("expenses component", () => {
+  test("download expense", () => {
+    render(
+      <Provider store={Store}>
+        <Expenses />
+      </Provider>
+    );
     
-    test('lable', () => {
-        render(<ForgetPassword/>);
-        const lableTxt = screen.getByText('Enter the email with which you have registered.');
-        expect(lableTxt).toBeInTheDocument();
-    })
-     
-    test('button', () => {
-        render(<ForgetPassword/>);
-        const buttonTxt = screen.getByText('Send Link');
-        expect(buttonTxt).toBeInTheDocument();
-    })
-    
-})
+    const activatePremiumbtn = screen.getByText('Activate Premium')
+    userEvent.click(activatePremiumbtn);
+
+    const downloadExpensesTxt = screen.getByText('Download Expenses');
+    expect(downloadExpensesTxt).toBeInTheDocument()
+  });
+
+  test('delete button', () => {
+    render(
+        <Provider store={Store}>
+            <Expenses/>
+        </Provider>
+    );
+
+    const deleteBtnTxt = screen.getByText('Delete');
+    expect(deleteBtnTxt).toBeInTheDocument();
+  })
+
+  test('edit button', () => {
+    render(
+        <Provider store={Store}>
+            <Expenses/>
+        </Provider>
+    );
+
+    const editBtnTxt = screen.getByText('Edit');
+    expect(editBtnTxt).toBeInTheDocument();
+  })
+});
