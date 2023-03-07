8418f2eceadbc74504455ff82f78e4c4f2cb1787
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-03-07 16:35:51 +0530

    test_driven_development

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import ForgetPassword from "./components/Auth/ForgetPassword";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('forget password', () => {
+    test('header', () => {
+        render(<ForgetPassword/>);
+        const headerTxt = screen.getByText('FORGET PASSWORD');
+        expect(headerTxt).toBeInTheDocument();
+    })
+    
+    test('lable', () => {
+        render(<ForgetPassword/>);
+        const lableTxt = screen.getByText('Enter the email with which you have registered.');
+        expect(lableTxt).toBeInTheDocument();
+    })
+     
+    test('button', () => {
+        render(<ForgetPassword/>);
+        const buttonTxt = screen.getByText('Send Link');
+        expect(buttonTxt).toBeInTheDocument();
+    })
+    
+})
